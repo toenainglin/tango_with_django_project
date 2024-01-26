890d2052bd486a10c5acd54001f317821642c469
Toe Naing Lin <2576694n@student.gla.ac.uk>
2024-01-26 00:11:59 +0000

    changed hyperlink text

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name ='about'),
 ]
 
--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("<p>Rango says hey there partner!</p> <a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("<p>Rango says here is the about page.</p> <a href='/rango/'>Index</a>")
